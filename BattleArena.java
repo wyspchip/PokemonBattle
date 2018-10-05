010a43792abce628c148ddede4ddc57f432f3bf9
John Kim <jkim@rowlandschools.org>
2018-10-05 07:34:29 -0700

    Initial sharing of project

---
+++ BattleArena.java
@@ -0,0 +1,28 @@
+class BattleArena
+{
+    
+    public static void main(String[] args)
+    {
+        Pokemon  squirtle = new Pokemon(120,8);
+        Pokemon  psyduck  = new Pokemon(200,5);
+        
+        System.out.println("squirtle health is "+squirtle.getHealth());
+        System.out.println("psyduck health is "+psyduck.getHealth());
+        
+        System.out.println("squirtle attacks psyduck");
+        psyduck.damaged( squirtle.getAttack() );
+        System.out.println("psyduck attacks squirtle");
+        squirtle.damaged( psyduck.getAttack() );
+        
+        System.out.println("squirtle health is "+squirtle.getHealth());
+        System.out.println("psyduck health is "+psyduck.getHealth());
+        
+        
+        
+        
+        
+        
+        
+    }
+}
+
---
+++ Pokemon.java
@@ -0,0 +1,64 @@
+/**
+ *  This is an amazing program that will win me lots of money
+ */
+
+class Pokemon
+{
+    /**
+     * Used for attacking
+     */
+    private int health;
+    private int attack;
+    
+    public Pokemon()
+    {
+        this.health = 100;
+        this.attack = 10;
+    }
+    
+    public Pokemon(int health, int attack)
+    {
+        this.health = health;
+        this.attack = attack;
+    }
+    
+    /**
+     * Accessors and Mutators
+     */
+    
+    public int getHealth()
+    {
+        return health;
+    }
+    
+    public void setHealth( int health)
+    {
+        this.health = health;
+    }
+    
+    public int getAttack()
+    {
+        return attack;
+    }
+    
+    public void setAttack( int attack )
+    {
+        this.attack = attack;
+    }
+    
+    
+    public void damaged(int attack)
+    {
+        health -= attack;
+        // You can also use
+        // this.health -= attack but there is only one health so there is no
+        // ambiguity
+    }
+    
+    
+}
+
+
+
+
+
